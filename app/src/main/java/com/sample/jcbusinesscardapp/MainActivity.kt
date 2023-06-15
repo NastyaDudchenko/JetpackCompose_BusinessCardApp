238626d18952ffbb683f56cec44ff76ebcab479c
anastasiia <anastasiia.dudchenko@onix-systems.com>
2023-06-15 22:50:44 +0300

    Created BusinessCard

--- app/src/main/java/com/sample/jcbusinesscardapp/MainActivity.kt
+++ app/src/main/java/com/sample/jcbusinesscardapp/MainActivity.kt
@@ -3,13 +3,33 @@ package com.sample.jcbusinesscardapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.sample.jcbusinesscardapp.ui.theme.JCBusinessCardAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +37,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JCBusinessCardAppTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    BusinessCard()
                 }
             }
         }
@@ -30,17 +49,93 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun BusinessCard() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(color = 0xFFd2e8d5))
+    ) {
+        Spacer(Modifier.weight(1f))
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(horizontal = 16.dp)
+        ) {
+            val image = painterResource(id = R.drawable.android_logo)
+            Image(
+                painter = image,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(90.dp)
+                    .background(Color(color = 0xFF073042))
+            )
+            Text(
+                text = stringResource(R.string.full_name),
+                fontWeight = FontWeight.Light,
+                fontSize = 36.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
+            Text(
+                text = stringResource(R.string.title),
+                color = Color(color = 0xFF006934),
+                fontWeight = FontWeight.Bold,
+                fontSize = 12.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp)
+            )
+        }
+        Spacer(Modifier.weight(1F))
+
+        val iconPhone = painterResource(id = R.drawable.ic_phone)
+        val iconShare = painterResource(id = R.drawable.ic_share)
+        val iconEmail = painterResource(id = R.drawable.ic_email)
+
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Bottom
+        ) {
+            InfoLine(text = stringResource(R.string.phone_number), icon = iconPhone)
+            InfoLine(text = stringResource(R.string.username), icon = iconShare)
+            InfoLine(text = stringResource(R.string.email), icon = iconEmail)
+        }
+        Spacer(Modifier.weight(weight = .1F))
+    }
+}
+
+@Composable
+fun InfoLine(text: String, icon: Painter) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 6.dp),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Spacer(Modifier.weight(1F))
+        Icon(
+            painter = icon,
+            contentDescription = null,
+            tint = Color(color = 0xFF006934),
+            modifier = Modifier.size(12.dp)
+        )
+        Spacer(Modifier.weight(weight = .2F))
+        Text(
+            text = text,
+            fontSize = 12.sp,
+            textAlign = TextAlign.Start,
+            modifier = Modifier.width(150.dp)
+        )
+        Spacer(Modifier.weight(1F))
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun BusinessCardPreview() {
     JCBusinessCardAppTheme {
-        Greeting("Android")
+        BusinessCard()
     }
 }
